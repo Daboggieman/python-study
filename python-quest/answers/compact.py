3e7a3ac80e2114449c520cc6b0406586ba2b6649
Daboggieman <adaraph722@gmail.com>
2026-07-15 10:59:34 +0100

    implement input handling and comparison logic for string processing tasks

--- python-quest/answers/compact.py
+++ python-quest/answers/compact.py
@@ -1,1 +1,17 @@
 # Write your solution here
+string_list_a = list(input("insert the list to sort through, seperate by comma: \n"))
+without_a = input("insert the characted/word to deduct count from the list: \n")
+def compact(string_list, without):
+    if len(string_list) < 1:
+        print("list is empty")
+        return []
+    count = 0
+    i = 0
+    while i < len(string_list):
+        if string_list[i] != without:
+            count += 1
+        i += 1
+    return count
+    print(f"the lenght of the list is {len(string_list)} \n")
+
+print(f"and there are {compact(string_list_a, without_a)} indexes without '{without_a}'")
--- python-quest/answers/compare.py
+++ python-quest/answers/compare.py
@@ -1,1 +1,16 @@
 # Write your solution here
+first_string = list(input("\033[32m insert the first string: \n \033[0m"))
+second_string = list(input("\033[32m insert the second string: \n \033[0m"))
+
+def compare(fir_str, oth_str):
+    while len(fir_str) != len(oth_str):
+        if len(fir_str) < len(oth_str):
+            fir_str.append(" ")
+        else: #len(fir_str) > len(oth_str):
+            oth_str.append(" ")
+    # print(fir_str)
+    # print(oth_str)
+    
+
+
+compare(first_string, second_string)
