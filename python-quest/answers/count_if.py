4df34813a31edd17ad4af475df47a86d0e91e2d7
Daboggieman <adaraph722@gmail.com>
2026-07-16 15:51:45 +0100

    solutions for count_if.py and countdown.py

--- python-quest/answers/count_if.py
+++ python-quest/answers/count_if.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def count_if(predicate, items) :
+    if len(items) < 1 :
+        print("items list is empty")
+        return None
+    check =list(map(predicate, items))
+    count = 0
+    for j in check :
+        if j == True :
+            count += 1
+    return count, check
+
+# the function is to take the 2 arguments passed through it 
+# and check for how many of the items in the second arguments list satisfies the condition in the first argument
+# and returns the amount of times the condition is satisfied through out the list of second argument
+
+print(count_if(lambda x: x > 2, [1, 2, 3, 4]))
--- python-quest/answers/countdown.py
+++ python-quest/answers/countdown.py
@@ -1,1 +1,21 @@
 # Write your solution here
+def countdown() :
+    j = []
+    for i in range (1, 10):
+        j.append(i)
+    k = j[::-1]
+
+    for nums in (k):
+        print(nums)
+
+
+#        OR
+
+
+    # i = 9
+    # while i > 0 :
+    #     print(i)
+    #     i -= 1
+
+
+countdown()
