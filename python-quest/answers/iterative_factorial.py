67ef36a4b3923df606d022d8c46209717324fa07
Daboggieman <adaraph722@gmail.com>
2026-07-21 14:01:41 +0100

    find

--- python-quest/answers/iterative_factorial.py
+++ python-quest/answers/iterative_factorial.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def iterative_factorial(n):
+    if n < 1 :
+        return 0
+    if n == 1:
+        return 1 
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+print(iterative_factorial(5))
